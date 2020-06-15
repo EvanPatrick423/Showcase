5915cec217bd0ad7de1fd697aa8d324569df5c21
EvanPatrick423 <EvanPatrick3@protonmail.com>
2020-06-15 14:16:56 -0400

    reconfiguring to work with psycopg2 for the server to work

--- showcase/showcase/settings.py
+++ showcase/showcase/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'showcase.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'showcase',
+	'USER': 'evan',
+	'PASSWORD': 'Helper423',
+	'HOST': 'localhost',
+	'PORT':'',
     }
 }
-
-#{
-    #'default': {
-    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #    'NAME': 'showcase',
-	#'USER': 'evan',
-	#'PASSWORD': 'Helper423',
-	#'HOST': 'localhost',
-	#'PORT':'',
-    #}
-#}
 
 
 # Password validation
