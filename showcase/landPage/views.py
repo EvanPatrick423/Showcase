8acff1e110c7189e89ba36261f1e28c718730ff1
EvanPatrick423 <EvanPatrick3@protonmail.com>
2021-02-16 17:03:58 -0500

    small unfinished update to landpage, adding resume and headshot as well

--- showcase/landPage/views.py
+++ showcase/landPage/views.py
@@ -5,3 +5,7 @@ from django.template import loader
 def index(request):
     template = loader.get_template('landPage/index.html')
     return render(request, 'landPage/index.html')
+
+def resume(request):
+    template = loader.get_template('landPage/static/landPage/Evan Patrick Resume Dev.pdf')
+    return render(request, 'landPage/static/landPage/Evan Patrick Resume Dev.pdf')
